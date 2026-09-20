60297e2ebde3f4589e76511c53a70ad024f86bf2
Aneela Shahzad <aneelashahzad991@gmail.com>
2026-09-20 12:43:44 +0500

    Add files via upload

---
+++ insertion sort.py
@@ -0,0 +1,13 @@
+def insertion_sort(li):
+    for x in range(1,len(li)-1):
+        key=li[x]
+        i=x-1
+        while i>-1 and li[i]>key:
+            li[i+1]=li[i]
+            i=i-1
+        li[i+1]=key
+
+num=[2,10,7,8,9,5,11,12]
+print("Unsorted :",num)
+insertion_sort(num)
+print("sorted :",num)
